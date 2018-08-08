95f99c15cbefb9dbaf856bfdb53e9e7e2af84dd3
Trzebu <trzebu@gmail.com>
2018-08-08 16:54:41 +0200

    Changing language method.

--- app/Http/Controllers/Home.php
+++ app/Http/Controllers/Home.php
@@ -2,14 +2,20 @@
 
 namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
-use Libs\DataBase\DataBase as DB;
 use Libs\Http\Request;
-use Libs\Session;
+use Libs\Translate;
+use Libs\Config;
 
 class Home extends Controller {
 
     public function index () {
+        $this->view->lang_list = Config::get("langs", true);
         $this->view->render("home.index");
+    }
+
+    public function language () {
+        Translate::changeLang(Request::input("lang"));
+        $this->redirect("home.index");
     }
 
 }
