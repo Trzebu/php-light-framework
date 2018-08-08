ba7224f2655a8ed43fbab09a01ee1cdad15a2b2c
Trzebu <trzebu@gmail.com>
2018-08-08 15:51:03 +0200

    Update PermissionsMiddleware.php

--- app/Http/Middleware/PermissionsMiddleware.php
+++ app/Http/Middleware/PermissionsMiddleware.php
@@ -13,21 +13,21 @@ namespace App\Http\Middleware;
 use Libs\User;
 use Libs\Http\Redirect;
 
-class AuthMiddleware {
-
-    private $_allowed_groups = [ //here you can add more groups with permit.
-        "admin",
-        "moderator",
-    ]
+class PermissionsMiddleware {
 
     public function __construct () {
+        $_allowed_groups = [ //here you can add more groups with permit.
+            "admin",
+            "moderator",
+        ];
         $has_permission = false;
-        foreach ($this->$_allowed_groups as $allowed) {
+
+        foreach ($_allowed_groups as $allowed) {
             if (User::permissions($allowed)) {
                 $has_permission = true;
             }
         }
-
+        
         if (!$has_permission) {
             Redirect::to("home.index");
         }
