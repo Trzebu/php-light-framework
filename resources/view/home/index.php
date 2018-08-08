01b2443c942b12f16ca479e910e713a566287a21
Trzebu <trzebu@gmail.com>
2018-08-08 15:57:50 +0200

    Update index.php

--- resources/view/home/index.php
+++ resources/view/home/index.php
@@ -4,9 +4,12 @@
     <li><a href="<?= route('auth.login') ?>">Login</a></li>
     <li><a href="<?= route('auth.register') ?>">Register</a></li>
     <?php if (Auth()->check()) { ?>
-    <li><a href="<?= route('user.profiles') ?>">Users list</a></li>
-    <li><a href="<?= route('user.profile', ["userId" => Auth()->data()->id]) ?>">My profile</a></li>
-    <li><a href="<?= route('auth.logout') ?>">Logout</a></li>
+        <li><a href="<?= route('user.profiles') ?>">Users list</a></li>
+        <li><a href="<?= route('user.profile', ["userId" => Auth()->data()->id]) ?>">My profile</a></li>
+        <li><a href="<?= route('auth.logout') ?>">Logout</a></li>
+        <?php if (Auth()->permissions("admin") || Auth()->permissions("moderator")) { ?>
+            <li><a href="<?= route('admin.index') ?>">Admin Panel</a></li>
+        <?php } ?>
     <?php } ?>
 </ul>
 
