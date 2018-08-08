8560bf3b3f6c7cafca6b618fdfb250e727f8c230
Trzebu <trzebu@gmail.com>
2018-08-08 16:57:07 +0200

    Update Controller.php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,6 +7,7 @@ use Libs\Validation\Validation;
 use Libs\Validation\ValidationErrors;
 use Libs\Session;
 use Libs\Http\Redirect;
+use Libs\Translate;
 
 class Controller {
 
@@ -14,6 +15,7 @@ class Controller {
         $this->view = new View();
         $this->view->token = new Token();
         $this->view->errors = new ValidationErrors();
+        $this->view->translate = new Translate();
     }
 
     protected function validation ($post, $filters) {
