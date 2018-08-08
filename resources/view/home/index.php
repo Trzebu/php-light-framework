a01f71d136bbe7c9c782080206630f09aa0884b6
Trzebu <trzebu@gmail.com>
2018-08-08 17:53:32 +0200

    Update index.php

--- resources/view/home/index.php
+++ resources/view/home/index.php
@@ -16,9 +16,10 @@
 </form>
 
 <ul>
-    <li><a href="<?= route('auth.login') ?>">Login</a></li>
-    <li><a href="<?= route('auth.register') ?>">Register</a></li>
-    <?php if (Auth()->check()) { ?>
+    <?php if (!Auth()->check()) { ?>
+        <li><a href="<?= route('auth.login') ?>">Login</a></li>
+        <li><a href="<?= route('auth.register') ?>">Register</a></li>
+    <?php } else { ?>
         <li><a href="<?= route('user.profiles') ?>">Users list</a></li>
         <li><a href="<?= route('user.profile', ["userId" => Auth()->data()->id]) ?>">My profile</a></li>
         <li><a href="<?= route('auth.logout') ?>">Logout</a></li>
