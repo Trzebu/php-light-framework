3c366150b51f8c58164b33a1bde2fdce3bda4d1f
Trzebu <trzebu@gmail.com>
2018-08-11 20:12:10 +0200

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -21,5 +21,6 @@ require_once(__ROOT__ . '/libs/functions/route.php');
 require_once(__ROOT__ . '/libs/functions/dd.php');
 require_once(__ROOT__ . '/libs/functions/auth.php');
 
+//Starting app
 $app = new Libs\App;
 
