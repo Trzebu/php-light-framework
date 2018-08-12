7d37ae5624ea55e02f9d06956c25441b2b25ffcb
Trzebu <trzebu@gmail.com>
2018-08-12 19:25:31 +0200

    Template compiler

--- libs/TemplateCompiler.php
+++ libs/TemplateCompiler.php
@@ -6,35 +6,63 @@ use Libs\Arr;
 
 class TemplateCompiler {
 
-    private $code = [];
+    private $_code = [];
+    private $_includes = [];
 
     public function __construct ($path) {
-        $this->code = explode("\n", file_get_contents($path));
+        $this->startCompile($path);
+        print_r($this->_code);
+    }
 
-        for ($i = 0; $i < count($this->code);) {
+    private function startCompile ($path) {
+        $this->_code = explode("\n", file_get_contents($path));
+        $this->cleareCode();
+        $this->compile();
+    }
 
-            $this->code[$i] = $this->removeSpace($this->code[$i]);
+    private function compile () {
+        for ($i = 0; $i < count($this->_code); $i++) {
 
-            if (strlen($this->code[$i]) == 1) {
-                unset($this->code[$i]);
+            if (strpos($this->_code[$i], "@") !== false) {
+                $command = ltrim($this->_code[$i], "@");
+                $params = explode(" ", $command);
+                $command = $params[0];
+                array_shift($params);
+
+                switch ($command) {
+                    
+                }
+
+            }
+
+        }
+    }
+
+    private function cleareCode () {
+        for ($i = 0; $i < count($this->_code);) {
+
+            $this->_code[$i] = $this->removeSpace($this->_code[$i]);
+
+            if (strlen($this->_code[$i]) == 1) {
+                unset($this->_code[$i]);
             } else {
                 $i++;
             }
 
-            $this->code = Arr::fixLowHigh($this->code);
+            $this->_code = Arr::fixLowHigh($this->_code);
         }
-
-        print_r($this->code);
     }
 
     private function removeSpace ($str) {
-        for ($j = 0; $j < strlen($str); $j++) {
-            if ($str[$j] != " ") {
+        for ($i = 0; $i < strlen($str); $i++) {
+            if ($str[$i] != " ") {
                 break;
             }
-            $str = ltrim($str, ' ');
+            $str = ltrim($str, " ");
         }
         return $str;
     }
+
+    
 
 }
