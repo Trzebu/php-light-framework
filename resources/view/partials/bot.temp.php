cb69dd6eefeea3f6e949bce6490a64a638e4dcbc
Trzebu <trzebu@gmail.com>
2018-08-14 17:18:20 +0200

    Update bot.temp.php

--- resources/view/partials/bot.temp.php
+++ resources/view/partials/bot.temp.php
@@ -2,7 +2,7 @@
 
         <footer class="page-footer font-small blue pt-4">
             <div class="footer-copyright text-center py-3">© 2018 Copyright:
-                <a href="https://github.com/Trzebu/php-light-framework"> Trzebu</a>
+                <a href="https://github.com/Trzebu/php-light-framework"> PHP Light Framework</a>
             </div>
         </footer>
 
