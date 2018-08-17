9972e298d8e28cdd9eee06d70340c0d19fe21727
Trzebu <trzebu@gmail.com>
2018-08-17 12:16:06 +0200

    Delete user.profiles.ctemp.php

--- storage/views/user.profiles.ctemp.php
+++
@@ -1,20 +0,0 @@
-<?php require_once("partials.top" . ".ctemp.php"); ?>
-<h1>Registered users:</h1>
-<?php if($this->data !== null): ?>
-<table class="table">
-<tr>
-<th scope="col">Login</th>
-<th scope="col">Profile</th>
-</tr>
-<?php foreach($this->data->results() as $user): ?>
-<tr>
-<td scope="row"><?php echo $user->login ?></td>
-<td><a href="<?php echo route('user.profile', ['userId' => $user->id]) ?>">View profile</a></td>
-</tr>
-<?php endforeach; ?>
-</table>
-<?php echo $this->data->paginateRender() ?>
-<?php else: ?>
-<h3>Currently we haven't registered users.</h3>
-<?php endif; ?>
-<?php require_once("partials.bot" . ".ctemp.php"); ?>
