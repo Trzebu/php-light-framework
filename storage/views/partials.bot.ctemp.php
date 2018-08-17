86ff2e03db60c6354baf711f05b1bf72545d9c5f
Trzebu <trzebu@gmail.com>
2018-08-17 12:16:57 +0200

    Delete partials.bot.ctemp.php

--- storage/views/partials.bot.ctemp.php
+++
@@ -1,8 +0,0 @@
-</div>
-<footer class="page-footer font-small blue pt-4">
-<div class="footer-copyright text-center py-3">© 2018 Copyright:
-<a href="https://github.com/Trzebu/php-light-framework"> PHP Light Framework</a>
-</div>
-</footer>
-</body>
-</html>
