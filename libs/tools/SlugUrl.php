3d1019131a1b7e6c182dbf0f373734c684f1860c
Trzebu <trzebu@gmail.com>
2018-09-06 22:14:01 +0200

    Update SlugUrl.php

--- libs/tools/SlugUrl.php
+++ libs/tools/SlugUrl.php
@@ -5,7 +5,7 @@ namespace Libs\Tools;
 class SlugUrl {
 
     public static function generate ($slug) {
-        $slug iconv('UTF-8', 'ASCII//TRANSLIT', $slug);
+        $slug = iconv('UTF-8', 'ASCII//TRANSLIT', $slug);
         $slug = preg_replace('/[^\-\s\pN\pL]+/u', '', mb_strtolower($slug, "UTF-8"));
         $slug = preg_replace('/[\-\s]+/', "-", $slug);
 
