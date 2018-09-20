d6e8adde99c22c5218d4673c51e215e4b9c21cf8
Trzebu <trzebu@gmail.com>
2018-09-20 09:27:00 +0200

    pl translations for new valid method.

--- resources/lang/pl/validation.php
+++ resources/lang/pl/validation.php
@@ -2,21 +2,29 @@
 
 return [
     "max" => [
-        "string" => ":attribute nie może mieć więcej niż :max znaków.",
         "numeric" => ":attribute nie może być większe niż :max.",
     ],
     "min" => [
-        "string" => ":attribute musi mieć więcej niż :min znaków.",
         "numeric" => ":attribute musi być większe niż :min.",
     ],
     "numeric" => ":attribute musi być liczbą.",
-    "string" => ":attribute musi być ciągiem znaków.",
-    "alpha" => ":attribute może zawierać tylko litery.",
-    "alpha_num" => ":attribute może zawierać tylko litery i cyfry.",
+    "string" => [
+        "str" => ":attribute musi być ciągiem znaków.",
+        "min" => ":attribute musi mieć więcej niż :min znaków.",
+        "max" => ":attribute nie może mieć więcej niż :max znaków."
+    ],
+    "alpha" => [
+        "letters" => ":attribute może zawierać tylko litery.",
+        "num" => ":attribute może zawierać tylko litery i cyfry.",
+    ],
+    "is_valid" => [
+        "email" => ":attribute musi być poprawnym adresem e-mail.",
+        "url" => ":attribute musi być poprawnym adresem URL.",
+    ],
     "same" => ":attribute i :other muszą być takie same.",
     "required" => ":attribute jest wymagany.",
     "unique" => ":attribute jest już zajęty.",
     "token" => "Token wygasł. Spróbuj ponownie.",
     "accepted" => ":attribute musi zostać zaakceptowany.",
-    "url" => ":attribute musi być poprawnym adresem URL.",
+    "image" => ":attribute musi być grafiką.",
 ];
