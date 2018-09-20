f42b826b9fdf00d4e2c4ff077502e086f9aad2dc
Trzebu <trzebu@gmail.com>
2018-09-20 12:25:34 +0200

    en translations for file size units.

---
+++ resources/lang/en/file_size_units.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+    "short" => [
+        0 => "B",
+        1 => "kB",
+        2 => "MB",
+        3 => "GB",
+        4 => "TB",
+        5 => "PB",
+        6 => "EB",
+        7 => "ZB",
+        8 => "YB"
+    ],
+    "full" => [
+        0 => "bytes",
+        1 => "kilobytes",
+        2 => "megabytes",
+        3 => "gigabytes",
+        4 => "terabytes",
+        5 => "petabytes",
+        6 => "exabytes",
+        7 => "zettabytes",
+        8 => "yottabytes",
+    ]
+];
