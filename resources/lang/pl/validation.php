173439faa624d0d3cfd6edd1ea494ed491d4e059
Trzebu <trzebu@gmail.com>
2018-09-20 14:01:34 +0200

    pl file rules translations.

--- resources/lang/pl/validation.php
+++ resources/lang/pl/validation.php
@@ -26,5 +26,18 @@ return [
     "unique" => ":attribute jest już zajęty.",
     "token" => "Token wygasł. Spróbuj ponownie.",
     "accepted" => ":attribute musi zostać zaakceptowany.",
-    "image" => ":attribute musi być grafiką.",
+    "image" => [
+        "forbidden_mime" => ":attribute nie może być obrazem typu :type",
+        "unknown_mime" => ":attribute jest nieznanego typu MIME.",
+        "resolution" => [
+            "max" => ":attribute nie może mieć większej rozdzielczości niż :max_width szerokości and :max_height wysokości.",
+            "min" => ":attribute nie może mieć mniejszej rozdzielczości niż :min_width szerokości i :min_height wysokości."
+        ],
+    ],
+    "file" => [
+        "size" => [
+            "min" => ":attribute nie może mieć więcej niż :min .",
+            "max" => ":attribute nie może mieć mniej niż :max .",
+        ],
+    ],
 ];
