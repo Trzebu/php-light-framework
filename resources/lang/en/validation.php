003c54a42637259140e49830f00dad4629340b1e
Trzebu <trzebu@gmail.com>
2018-09-20 14:01:12 +0200

    en file rules translations.

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -26,5 +26,18 @@ return [
     "unique" => "The :attribute has already been taken.",
     "token" => "Token expired. Try again.",
     "accepted" => "The :attribute must be accepted.",
-    "image" => "The :attribute must be an image.",
+    "image" => [
+        "forbidden_mime" => "The :attribute can not be a :type image.",
+        "unknown_mime" => "The :attribute have unknown MIME type.",
+        "resolution" => [
+            "max" => "The :attribute resolution can not exceed :max_width width and :max_height height.",
+            "min" => "The :attribute resolution can not be less than :min_width width and :min_height height."
+        ],
+    ],
+    "file" => [
+        "size" => [
+            "min" => "The :attribute must be at least :min .",
+            "max" => "The :attribute may not be greater than :max .",
+        ],
+    ],
 ];
