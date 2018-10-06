7051ce015dd3a4bd30908e3ca574bea2e160471d
Szymon Fox <trzebu@gmail.com>
2018-10-06 14:16:31 +0200

    Quick token using.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,6 +6,6 @@ require_once(__ROOT__ . '/libs/functions/route.php');
 require_once(__ROOT__ . '/libs/functions/dd.php');
 require_once(__ROOT__ . '/libs/functions/auth.php');
 require_once(__ROOT__ . '/libs/functions/trans.php');
+require_once(__ROOT__ . '/libs/functions/token.php');
 
 $app = new Libs\App;
-
